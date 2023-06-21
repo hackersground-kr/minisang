fd3f7b7189fa343104f32d7cf0dce426ae6e8ec9
minisangmini <sangmin5915@gmail.com>
2023-06-21 22:02:21 +0900

    backend init

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+
+// packages
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('hi');
+})
+
+export default app;
