d6258af954c93c201340014a2ab4912a4e28e5a9
minisangmini <sangmin5915@gmail.com>
2023-06-22 00:04:48 +0900

    add cors

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,11 @@ const app = express();
 // packages
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import cors from 'cors';
 
 
 app.use(helmet());
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
