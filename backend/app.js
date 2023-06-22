0c787c69f5b66399f19cf32e55f67a0faf023b78
minisangmini <sangmin5915@gmail.com>
2023-06-22 15:01:22 +0900

    actiob test

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,13 @@ const app = express();
 
 // packages
 import bodyParser from 'body-parser';
-import helmet from 'helmet';
-import cors from 'cors';
-
-
-app.use(helmet());
-app.use(cors());
+ 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('hi ~');
+  res.send('hi test~');
 })
 
 app.post('/', (req, res) => {
